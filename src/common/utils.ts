b9e1c670b14fbb10d7e233d83af38f5f6a0f83fa
Kubas556 <Kubas556@users.noreply.github.com>
2025-03-24 16:08:48 +0100

    added timeout

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,24 +1,35 @@
 function waitForAllQuerySelectors(selectors: string[]) {
   type returnType = Element[][];
   type queryType = (Element[] | null)[];
-  return new Promise<returnType>((res, _) => {
-    new Promise<{ id: number; data: returnType }>((resolveInterval, _) => {
-      const id = setInterval(() => {
-        const resolved: queryType = [];
-        for (const selector of selectors) {
-          const queryRes = document.querySelectorAll(selector);
-          resolved.push(
-            queryRes && queryRes.length > 0 ? Array.from(queryRes) : null
-          );
-        }
-        if (resolved.indexOf(null) === -1) {
-          resolveInterval({ id: id, data: resolved as returnType });
-        }
-      }, 20);
-    }).then(({ id, data }) => {
-      clearInterval(id);
-      res(data);
-    });
+  return new Promise<returnType>((res, rej) => {
+    new Promise<{ id: number; data: returnType }>(
+      (resolveInterval, rejectInterval) => {
+        const id = setInterval(() => {
+          const resolved: queryType = [];
+          for (const selector of selectors) {
+            const queryRes = document.querySelectorAll(selector);
+            resolved.push(
+              queryRes && queryRes.length > 0 ? Array.from(queryRes) : null
+            );
+          }
+          if (resolved.indexOf(null) === -1) {
+            resolveInterval({ id: id, data: resolved as returnType });
+          }
+        }, 20);
+        setTimeout(() => {
+          rejectInterval({ id });
+        }, 8000);
+      }
+    ).then(
+      ({ id, data }) => {
+        clearInterval(id);
+        res(data);
+      },
+      ({ id }) => {
+        clearInterval(id);
+        rej();
+      }
+    );
   });
 }
 
